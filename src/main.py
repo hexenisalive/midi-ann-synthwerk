5546d4bd2cf06b8f85dbc3651f76772d05ba0645
hexenisalive <6stringbastard@gmail.com>
2020-03-13 17:16:20 +0100

    delete commented area in main

--- src/main.py
+++ src/main.py
@@ -78,6 +78,16 @@ def prepare_tensors():
     return batch_tensor
 
 
+def test_stream_to_file():
+    # randomly generated simulated data for testing stream generation
+    test_output_data = []
+    for no_of_notes in range(128):
+        test_output_data.append([uniform(-1, 3), uniform(-0.5, 0.5), no_of_notes * 0.5])
+
+    stream = data_to_stream(test_output_data)
+    stream_to_file(stream, "../test_output.mid")
+
+
 if __name__ == "__main__":
     # prepare_data for : chopin / beeth / tschai
     prepare_data('../MIDIs/tschai/')
@@ -85,21 +95,13 @@ if __name__ == "__main__":
 
     input_data = prepare_tensors()
 
-    """ 
-    
+    # unfinished model
     model = tf.keras.Sequential()
     model.add(tf.keras.layers.LSTM(units=64, input_shape=(128, 3), return_sequences=False))
     
     model.summary()
-    """
 
-    """
-    # randomly generated output data for testing stream generation
-    test_output_data = []
-    for no_of_notes in range(128):
-        test_output_data.append([uniform(-1, 3), uniform(-0.5, 0.5), no_of_notes*0.5])
-    
-    stream = data_to_stream(test_output_data)
-    stream_to_file(stream, "../test_output.mid")
-    
-    """
+    # model.fit(input_data, input_data)
+
+
+
