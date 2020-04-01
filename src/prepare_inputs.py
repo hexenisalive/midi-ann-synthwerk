15c2ac0e1e32ebd7636e1f71a340c41e1fc083b6
hexenisalive <6stringbastard@gmail.com>
2020-04-01 16:05:25 +0200

    delete redundant iterator

--- src/prepare_inputs.py
+++ src/prepare_inputs.py
@@ -15,7 +15,7 @@ def prepare_tensors():
     for sequence, offset in zip(sequence_data, offset_data):
         tensor_list = []
         curr_offset = 0.0
-        for i, (element_s, element_o) in enumerate(zip(sequence, offset)):
+        for element_s, element_o in zip(sequence, offset):
             tensor_list.append(tf.Variable([
                 master_dict[element_s]["coords"][0],
                 master_dict[element_s]["coords"][1],
@@ -25,7 +25,7 @@ def prepare_tensors():
         for trim in range(ceil(len(tensor_list)/128)):
             if (trim + 1) * 128 > len(tensor_list):
                 batch_tensor_list.append(tf.stack(
-                    tensor_list[len(tensor_list)-128 : len(tensor_list)]))
+                    tensor_list[len(tensor_list)-128: len(tensor_list)]))
             else:
                 batch_tensor_list.append(tf.stack(tensor_list[0 + (trim * 128) : 128 + (trim * 128)]))
     batch_tensor = tf.stack(batch_tensor_list)
